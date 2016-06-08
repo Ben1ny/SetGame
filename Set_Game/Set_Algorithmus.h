fe78f485ae9013eba837c4386c803dd3eb1dec4d
Ben <freakimkaefig87@gmx.de>
2016-06-08 22:03:37 +0200

    Anpassung der Commits

--- Set_Game/Set_Algorithmus.h
+++ Set_Game/Set_Algorithmus.h
@@ -16,14 +16,14 @@ class Set_Algorithmus
 public:
 	Set_Algorithmus();
 	~Set_Algorithmus();
-
+	//Methodes to handle the Game
 	bool CheckBuildUp(array <Set_Card, 15>);
 	void BuildtheDeck(array <Set_Card, 15>, CSet_GameDlg *);
 	void BuildtheDeckThreeMore(array <Set_Card, 15>, CSet_GameDlg *);
 	bool CheckForSet(Set_Card, Set_Card, Set_Card);
 	void ThreeButtonsSet(Set_Deck &, array <Set_Card, 15> &, Set_Card &, int, int, CSet_GameDlg *);
 	void GetThreeMore(Set_Deck &, array <Set_Card, 15> &, CSet_GameDlg *);
-
+	// Set and Get Methodes
 	CString getName(int);
 	void setName(int, CString);
 	int getPoints(int);
@@ -38,7 +38,7 @@ public:
 	void resetPoints();
 
 private:
-
+	//Arrays for the game
 	bool *threeontop;
 	int *points;
 	int *result;
@@ -46,7 +46,7 @@ private:
 	Set_Card *auswahl_check;
 	CString *spieler_name;
 	int *buttons_array;
-
+	//Special variables and flags
 	bool threemoreflag;
 	bool endgame;
 	int lastbutton;
